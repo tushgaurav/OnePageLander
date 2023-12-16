52cc9841c2b8a3633dcae6b6b601cc7a4a7bd0c2
Tushar Gaurav <tushar.gaurav416@gmail.com>
2023-12-16 18:39:57 +0530

    updated moto

--- components/hero.tsx
+++ components/hero.tsx
@@ -65,7 +65,7 @@ export default function Hero() {
                   data-aos="fade-up"
                   data-aos-delay="200"
                 >
-                  AI generated tailored robots and enabler of turn-key Industrial Automation.
+                  AI generated tailored robots and enabler of turn-key industrial automation combined with instant solutioning.
                 </p>
               </div>
 
